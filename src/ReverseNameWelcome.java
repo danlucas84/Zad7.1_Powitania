e4cb58df6ab087ae207dfe2e8e1e69cf6844c930
Dell <dandan_21@o2.pl>
2019-05-24 13:15:11 +0200

    rozwiazanie zawierające ouput zgodny z trescia zadania

---
+++ src/ReverseNameWelcome.java
@@ -0,0 +1,45 @@
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
+public class ReverseNameWelcome {
+
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        String names[] = new String[5];
+
+
+        System.out.println("Podaj 1 imie");
+        names[0] = input.nextLine();
+
+        System.out.println("Podaj 2 imie");
+        names[1] = input.nextLine();
+
+        System.out.println("Podaj 3 imie");
+        names[2] = input.nextLine();
+
+        System.out.println("Podaj 4 imie");
+        names[3] = input.nextLine();
+
+        System.out.println("Podaj 5 imie");
+        names[4] = input.nextLine();
+
+
+        for (int i = names.length-1; i>=0; i--) {
+
+            System.out.println("Czesc " + names[i] + "." );
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
